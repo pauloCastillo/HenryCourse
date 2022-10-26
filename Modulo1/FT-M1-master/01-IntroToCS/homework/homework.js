971bc4f9a310aadeb1327dde776e4ba6e03d1330
Paulo Castillo <xpropollo@gmail.com>
2022-10-26 16:10:58 -0400

    finish homework

--- Modulo1/FT-M1-master/01-IntroToCS/homework/homework.js
+++ Modulo1/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,15 +1,29 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  let decimal = 0
+  let numReverse = num.split('').reverse().join('')
 
+  for (let i = 0; i < numReverse.length; i++) {
+    decimal += numReverse[i] * 2 ** i;
+  }
+  return decimal;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let result = ''
+  while(num >= 1){
+    if(num % 2 == 0){
+      result += '0'
+    }else if(num % 2 == 1){
+      result += '1'
+    }
+    num /= 2
+  }
+  let binarioNumber = result.split("").reverse().join("")
+  return binarioNumber
 }
-
+console.log(BinarioADecimal('100'))
 
 module.exports = {
   BinarioADecimal,
